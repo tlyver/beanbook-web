c5b8c112b38728327b8c5a86cef9637a3944dc5d
tlyver <tjlyver@gmail.com>
2019-11-02 10:58:37 -0400

    Sets navbar
    Imports bean bool club photo.
    Adds navbar to App level.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <navbar/>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="sass">
 @import "@/sass/custom.sass"
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img alt="Vue logo" src="../assets/bean.png">
+      </a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          About
+        </a>
+
+        <a class="navbar-item">
+          Our Picks
+        </a>
+
+        <a class="navbar-item">
+          Contact
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
