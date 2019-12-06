b3b12cac1c0724456b9dc1d9409bd5176bde301c
Thomas Lyver <tjlyver@gmail.com>
2019-12-06 14:45:23 -0500

    Loading cursor
    Creates cursor component.
    Adds timer for cursor to make blinking effect.

---
+++ src/components/BeanCursor.vue
@@ -0,0 +1,36 @@
+<template>
+  <font-awesome-icon v-if="showIcon" icon="i-cursor" />
+  <font-awesome-icon v-else class="has-text-white" icon="i-cursor" />
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faICursor
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faICursor
+)
+
+export default {
+  name: 'BeanCursor',
+  data () {
+    return {
+      showIcon: true
+    }
+  },
+  mounted () {
+    this.iconCounter()
+  },
+  methods: {
+    toggleIcon () {
+      this.showIcon = !this.showIcon
+      this.iconTimer()
+    },
+    iconTimer () {
+      setTimeout(this.toggleIcon, 400)
+    }
+  }
+}
+</script>
