83d6c6cd302e8c68ab90ca2041293fdddc12f07f
Thomas Lyver <tjlyver@gmail.com>
2019-12-18 03:04:29 -0500

    Fixes root element
    Adds div as root element.

--- src/components/BeanNook.vue
+++ src/components/BeanNook.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="nook">
-    <h4 class="title is-4 has-text-primary">
-      {{ format(new Date(nook.featured_date), 'MMMM') }}
-      {{ `'${format(new Date(nook.featured_date), 'yy')}` }}
-    </h4>
-  </div>
-  <div class="">
-    <div class="nook-summary">
-      <span class="has-text-weight-bold has-text-primary">{{ nook.name}}</span>
-      {{ `- ${nook.commentary}` }}
+  <div>
+    <div class="nook">
+      <h4 class="title is-4 has-text-primary">
+        {{ format(new Date(nook.featured_date), 'MMMM') }}
+        {{ `'${format(new Date(nook.featured_date), 'yy')}` }}
+      </h4>
     </div>
-    <figure class="image">
-      <img alt="Pick" :src="nook.images[0].url" v-if="nook.images[0]">
-      <bean-cursor v-else/>
-    </figure>
+    <div class="">
+      <div class="nook-summary">
+        <span class="has-text-weight-bold has-text-primary">{{ nook.name}}</span>
+        {{ `- ${nook.commentary}` }}
+      </div>
+      <figure class="image">
+        <img alt="Pick" :src="nook.images[0].url" v-if="nook.images[0]">
+        <bean-cursor v-else/>
+      </figure>
+    </div>
   </div>
 </template>
 
