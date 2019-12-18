ccdd3443c31e7384c18b12651424d2c938fdfd7b
Thomas Lyver <tjlyver@gmail.com>
2019-12-17 20:17:21 -0500

    Imports date-fns package. Adds bean-pick component. Adds styles and media queries.

---
+++ src/components/BeanPick.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <div class="bookshelf">
+      <h4 class="title is-4 has-text-primary">
+        {{ format(new Date(result.featured_date), 'MMMM') }}
+        {{ `'${format(new Date(result.featured_date), 'yy')}` }}
+      </h4>
+      <div class="columns">
+        <div class="img column is-narrow">
+          <figure class="image">
+            <img alt="Pick" :src="thumbnail" v-if="thumbnail">
+            <bean-cursor v-else/>
+          </figure>
+        </div>
+        <div class="column is-full">
+          <div class="title-box">
+            <span class="has-text-weight-bold has-text-primary">
+              {{ `"${result.title}"` }}
+            </span>
+            <span v-for="author in result.authors" :key="author.full_name">{{ `by ${author.full_name}` }}</span>
+          </div>
+          <div class="info-box">
+            <div class="categories">
+              <span class="info-label has-text-left has-text-grey">genres:</span>
+              <div
+                class="tag is-primary is-light has-text-weight-bold is-lowercase"
+                v-for="category in result.categories"
+                :key="category.name">
+                {{ category.name }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="description">
+        <div v-if="apiRes.volumeInfo">
+          <div class="book-description" v-html="apiRes.volumeInfo.description"></div>
+          <div v-if="descriptionExpanded">
+            <button class="button is-outlined is-primary" type="button" name="loadMore">More</button>
+          </div>
+        </div>
+        <bean-cursor v-else/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import BeanCursor from '@/components/BeanCursor.vue'
+import { format } from 'date-fns'
+
+export default {
+  props: ['result'],
+  components: {
+    BeanCursor
+  },
+  data () {
+    return {
+      format,
+      apiRes: {},
+      thumbnail: null,
+      smallThumbnail: null,
+      descriptionExpanded: null
+    }
+  },
+  created () {
+    this.getGoogleApi()
+  },
+  methods: {
+    checkDescriptionLength (description) {
+      console.log(description.length)
+      if (description.length > 10) {
+        this.descriptionExpanded = true
+      } else {
+        return false
+      }
+    },
+    setThumnails () {
+      const { imageLinks } = this.apiRes.volumeInfo
+      this.smallThumbnail = imageLinks.smallThumbnail
+      this.thumbnail = imageLinks.thumbnail
+    },
+    async getGoogleApi () {
+      try {
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.result.isbn_10}`)
+        this.apiRes = response.data.items[0]
+        this.setThumnails()
+        this.checkDescriptionLength()
+      } catch (error) {
+        // handle error
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import "@/sass/custom.sass"
+  @import "~bulma/bulma"
+  +until(550px)
+    .columns
+      width: 100%
+    .img
+      display: flex
+      justify-content: center
+  .bookshelf
+    display: flex
+    flex-direction: column
+    align-items: flex-start
+    margin-bottom: 2rem
+  .img-box
+    display: flex
+    flex-direction: column
+    border: 1px solid #000
+    align-items: center
+    justify-content: center
+    width: 200px
+    height: 200px
+  .title-box
+    text-align: left
+  .info-box
+    display: flex
+    flex-direction: column
+    align-items: flex-start
+    .author
+      display: flex
+  .info-label
+    padding-right: .5rem
+  .categories
+  .description
+    display: flex
+    flex-direction: column
+    align-items: flex-start
+  .book-description
+    text-align: left
+  .tag
+    margin-right: 0.5rem
+</style>
--- src/views/Bookshelf.vue
+++ src/views/Bookshelf.vue
@@ -1,213 +1,73 @@
 <template>
   <div>
     <div class="page-title">
-      <h1 class="title is-2">
+      <h1 class="title is-2 has-text-primary">
         The Bookshelf
       </h1>
-      <h5 class="subtitle">
+      <h5 class="subtitle has-text-primary">
         Past and present.
       </h5>
     </div>
-    <div class="bookshelf" v-for="result in results" :key="result.uuid">
-      <h4 class="title is-4">{{ result.featured_date }}</h4>
-      <div class="columns is-mobile">
-        <div class="column is-narrow">
-          <div class="img-box">
-            <div class="">  ^^^^^  </div>
-            <div class="">(  o o )</div>
-            <div class=""> --- </div>
-          </div>
-        </div>
-        <div class="column is-full">
-          <div class="title-box">
-            <div class="has-text-weight-bold">
-              {{ result.title }}
-            </div>
-            <div v-if="result.sub_title !== null">
-              {{ result.sub_title }}
-            </div>
-          </div>
-          <div class="info-box">
-            <div class="author">
-              <div class="info-label has-text-grey">
-                <span v-if="result.authors.length > 1">authors: </span>
-                <span v-else>author: </span>
-              </div>
-              <span v-for="author in result.authors" :key="author.full_name">{{ author.full_name }}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="description">
-        <div class="has-text-weight-bold is-uppercase">
-          About {{ result.title }}
-        </div>
-        <div v-if="apiRes.volumeInfo" class="book-description" v-html="apiRes.volumeInfo.description">
-        </div>
-      </div>
-      <!-- <div class="columns">
-        <div class="columm">
-          <img alt="Bean Books" src="../assets/bean.png">
-        </div>
-        <div class="column">
-
-        </div>
-      </div> -->
-    </div>
+    <bean-pick v-for="result in results" :key="result.uuid" :result="result"/>
   </div>
 </template>
 <script>
 import axios from 'axios'
+import BeanPick from '@/components/BeanPick.vue'
+
 export default {
+  components: {
+    BeanPick
+  },
   data () {
     return {
-      apiRes: {},
-      results: [],
-      res: {
-        count: 4,
-        next: null,
-        previous: null,
-        results: [
-          {
-            uuid: '370e4963-53a3-4d2c-be98-16e39283499d',
-            created_on: '2019-11-18T01:07:26.534330Z',
-            updated_on: '2019-11-18T01:07:26.534366Z',
-            title: 'War and Peace',
-            sub_title: 'The story of War and Peace',
-            authors: [
-              { full_name: 'Leo Tolstoy' }
-            ],
-            release_date: '1869-01-01',
-            categories: [
-              { name: 'fiction' },
-              { name: 'history' }
-            ],
-            isbn_10: '9780854566242',
-            isbn_13: null,
-            featured_date: '2019-10-01',
-            rating: null,
-            commentary: null
-          },
-          {
-            uuid: '8b5dfc0c-dee0-4472-89a8-e99943f0979e',
-            created_on: '2019-11-18T01:07:26.540603Z',
-            updated_on: '2019-11-18T01:07:26.540672Z',
-            title: 'War and Peace',
-            sub_title: null,
-            authors: [
-              { full_name: 'Leo Tolstoy' }
-            ],
-            release_date: '1869-01-01',
-            categories: [
-              { name: 'fiction' },
-              { name: 'history' }
-            ],
-            isbn_10: '9780854566242',
-            isbn_13: null,
-            featured_date: '2019-09-01',
-            rating: null,
-            commentary: null
-          },
-          {
-            uuid: '2fa59135-6130-4f6b-aa4f-27d6a092fea2',
-            created_on: '2019-11-18T01:07:26.546379Z',
-            updated_on: '2019-11-18T01:07:26.546415Z',
-            title: 'War and Peace',
-            sub_title: null,
-            authors: [
-              { full_name: 'Leo Tolstoy' }
-            ],
-            release_date: '1869-01-01',
-            categories: [
-              { name: 'fiction' },
-              { name: 'history' }
-            ],
-            isbn_10: '9780854566242',
-            isbn_13: null,
-            featured_date: '2019-08-01',
-            rating: null,
-            commentary: null
-          },
-          {
-            uuid: '6a86bb8c-7329-4504-8782-08405d283419',
-            created_on: '2019-11-18T01:07:26.554086Z',
-            updated_on: '2019-11-18T01:07:26.554137Z',
-            title: 'War and Peace',
-            sub_title: null,
-            authors: [
-              { full_name: 'Leo Tolstoy' }
-            ],
-            release_date: '1869-01-01',
-            categories: [
-              { name: 'fiction' },
-              { name: 'history' }
-            ],
-            isbn_10: '9780854566242',
-            isbn_13: null,
-            featured_date: '2019-07-01',
-            rating: null,
-            commentary: null
-          }
-        ]
-      }
+      results: []
+      // res: {
+      //   count: 4,
+      //   next: null,
+      //   previous: null,
+      //   results: [
+      //     {
+      //       uuid: '370e4963-53a3-4d2c-be98-16e39283499d',
+      //       created_on: '2019-11-18T01:07:26.534330Z',
+      //       updated_on: '2019-11-18T01:07:26.534366Z',
+      //       title: 'War and Peace',
+      //       sub_title: 'The story of War and Peace',
+      //       authors: [
+      //         { full_name: 'Leo Tolstoy' }
+      //       ],
+      //       release_date: '1869-01-01',
+      //       categories: [
+      //         { name: 'fiction' },
+      //         { name: 'history' }
+      //       ],
+      //       isbn_10: '0156787334',
+      //       isbn_13: null,
+      //       featured_date: '2019-10-01',
+      //       rating: null,
+      //       commentary: null
+      //     }
+      //   ]
+      // }
     }
   },
-  created () {
-    this.getGoogleApi()
-  },
   mounted () {
+    this.getBooks()
     this.results = this.res.results
   },
   methods: {
-    getBook () {
-      window.alert('hello')
-    },
-    async getGoogleApi () {
-      await axios.get('https://www.googleapis.com/books/v1/volumes/dXewCgAAQBAJ')
-        .then((response) => {
-          this.apiRes = response.data
-        })
-        .catch(function (error) {
-          console.log(error)
-        })
+    async getBooks () {
+      try {
+        const res = await axios.get('beanbook.club/api/club/books')
+        console.log(res)
+        this.results = res.data
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
 </script>
 
 <style lang="sass" scoped>
-  .bookshelf
-    display: flex
-    flex-direction: column
-    align-items: flex-start
-    margin-bottom: 2rem
-  .img-box
-    display: flex
-    flex-direction: column
-    border: 1px solid #000
-    align-items: center
-    justify-content: center
-    width: 200px
-    height: 200px
-  .title-box
-    display: flex
-    flex-direction: column
-    align-items: flex-start
-    justify-content: flex-start
-  .info-box
-    display: flex
-    flex-direction: column
-    align-items: flex-start
-    .author
-      display: flex
-  .info-label
-    padding-right: .5rem
-  .column
-    // background-color: #000
-  .description
-    display: flex
-    flex-direction: column
-    align-items: flex-start
-  .book-description
-    text-align: left
 </style>
