2baf89550cae654ace74424464b325207251ec26
Thomas Lyver <tjlyver@gmail.com>
2020-01-15 22:18:23 -0500

    About page description
    Updates description.

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,8 +10,7 @@
     </div>
     <div class="page-description">
       <p>
-        Books are a HUGE part of my life. I fell in love with reading when I was younger and I believe it was my love of reading and storytelling that led me to the work that I do now.
-  I thought about starting a book club in late December, when I was still navigating what a social life looked like post grad (a book club is my kind of social scene). I wanted to create a space where women could get together, share stories, and build connections through the books that challenge us, make us laugh or cry, and allow us to think or see things differently. I am thrilled to be here and I hope this book club becomes yours, as well.
+        Est. 2019
       </p>
     </div>
   </div>
